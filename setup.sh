bf14ace54e8aa1d7ca97950137f5d4a18e4b500c
Rishab Gargeya <rishab.gargeya@gmail.com>
2018-07-11 03:55:36 +0900

    setup for gcp running

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+conda install -c conda-forge opencv=2.4
+sudo apt install libgl1-mesa-glx
+sudo apt-get install -y libxrender-dev
+dpkg --add-architecture i386
+sudo apt-get install lib32gcc1
+sudo apt update && apt install -y libsm6 libxext6
+sudo apt-get update
