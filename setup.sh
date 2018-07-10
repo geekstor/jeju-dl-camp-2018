59b5c7bcab086f55e0d2dfa0f37d0e2d0d4fb0f3
rishabgargeya <rishab.gargeya@gmail.com>
2018-07-10 19:41:47 +0000

    run script change

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ sudo apt update && apt install -y libsm6 libxext6
 sudo apt-get install xvfb
 sudo apt-get install libav-tools
 sudo apt-get install htop
+sudo apt-get install moreutils
 sudo apt-get update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
