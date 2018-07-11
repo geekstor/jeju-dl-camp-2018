13c12a5724e6aefa8178796bfed11f223239cccf
rishabgargeya <rishab.gargeya@gmail.com>
2018-07-11 01:54:03 +0000

    fix run command for gcp

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup xvfb-run -s "-screen 0 1400x900x24" python3 -m trainer.train params_sonic.json | ts >> 'log2.out' 2>&1 &
+nohup xvfb-run -s "-screen 0 1400x900x24" python3 -u -m trainer.train params_sonic.json >> 'log.out' 2>&1 &
