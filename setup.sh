efc5cec82675e3112fd0100fe5dce3448f8ba208
Rishab Gargeya <rishab.gargeya@gmail.com>
2018-07-25 05:21:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt install libgl1-mesa-glx
-sudo apt-get install -y libxrender-dev
-dpkg --add-architecture i386
-sudo apt-get install lib32gcc1
-sudo apt update && apt install -y libsm6 libxext6
-sudo apt-get install xvfb
-sudo apt-get install libav-tools
-sudo apt-get install htop
-sudo apt-get install moreutils
-sudo apt-get update
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-sleep 10s
-source ~/.bashrc
-sleep 10s
-conda env create
-sleep 10s
-python -m retro.import.sega_classics
-
-
-
-
-
-
-
-
-#!/bin/bash
 rm -rf $HOME/miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
@@ -39,16 +11,16 @@ source ~/.bashrc
 sleep 10s
 
 sudo apt-get update
-sudo apt install libgl1-mesa-glx
+sudo apt install -y libgl1-mesa-glx
 sudo apt-get install -y libxrender-dev
 dpkg --add-architecture i386
-sudo apt-get install lib32gcc1
+sudo apt-get install -y lib32gcc1
 sudo apt update && apt install -y libsm6 libxext6
-sudo apt-get install python-opengl
-sudo apt-get install xvfb
-sudo apt-get install libav-tools
-sudo apt-get install htop
-sudo apt-get install moreutils
+sudo apt-get install -y python-opengl
+sudo apt-get install -y xvfb
+sudo apt-get install -y libav-tools
+sudo apt-get install -y htop
+sudo apt-get install -y moreutils
 sudo apt-get update
 
 sleep 5s
