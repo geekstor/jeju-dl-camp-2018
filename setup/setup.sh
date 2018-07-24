3e1c961a8566247c0c5f91722c65c79578d634f0
Rishab Gargeya <rishab.gargeya@gmail.com>
2018-07-25 03:24:44 +0900

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,12 +4,27 @@ sh get-docker.sh
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 
+sudo apt-get update
+sudo apt install libgl1-mesa-glx
+sudo apt-get install -y libxrender-dev
+dpkg --add-architecture i386
+sudo apt-get install lib32gcc1
+sudo apt update && apt install -y libsm6 libxext6
+sudo apt-get install python-opengl
 sudo apt-get install xvfb
 sudo apt-get install libav-tools
-sudo apt-get install python-opengl
+sudo apt-get install htop
+sudo apt-get install moreutils
+sudo apt-get update
 
 sleep 10s
 source ~/.bashrc
+sleep 10s
+
+wget https://raw.githubusercontent.com/fastai/courses/master/setup/install-gpu.sh
+sudo sh install-gpu.sh
+
+
 sleep 10s
 conda env create
 
